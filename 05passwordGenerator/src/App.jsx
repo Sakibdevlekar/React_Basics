598f37a34ea887b9026109226c4531f7bf225905
sakibdevlekar <sakibdevlekar96@gmail.com>
2023-12-19 13:07:39 +0530

    added state change to copy button

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 function App() {
   const [length, setLength ] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
+  const [changeText, setText] = useState('Copy');
+  const [color, setColor] = useState('#187bcd');
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState('');
   const passwordRef = useRef(null)
@@ -25,8 +27,14 @@ function App() {
   }, [length, numberAllowed, charAllowed]);
 
   const copyPasswordToClipboard = () =>{
+    setText('Copied!');
+    setColor('#00b300');
     window.navigator.clipboard.writeText(password);
-    passwordRef.current?.select()
+    passwordRef.current?.select();
+    setTimeout(()=>{
+      setText('copy');
+      setColor('#187bcd');
+    },2000)
   }
   return (
     <>
@@ -41,7 +49,8 @@ function App() {
         ref={passwordRef}
         />
         <button onClick = {copyPasswordToClipboard}
-        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+        className='outline-none  text-white px-3 py-0.5 shrink-0'style={{background:color}}>{changeText}</button>
+        
       </div>
       <div className='flex items-center gap-x-2 mt-4'>
         <input type="range"

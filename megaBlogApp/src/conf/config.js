6823528273805610393f2627ffd42aa9539d0bcf
sakibdevlekar <sakibdevlekar96@gmail.com>
2024-03-31 14:57:09 +0530

    final

--- megaBlogApp/src/conf/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  appWriteUrl: import.meta.env.VITE_APPWRITE_URL,
-  appProjectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-  appDatabaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-  appCollectionId: import.meta.env.VITE_APPWRITE_COLLECTION_ID,
-  apBucketId: import.meta.env.VITE_APPWRITE_BUCKET_ID,
-};
-
-export default config;
